fd4f6bbf570d05f0a312a09572afceaa753fa7f1
DarkMannn <mdarko1993@gmail.com>
2019-09-10 08:59:44 +0200

    Refactored and improved App.js

--- README.md
+++ README.md
@@ -8,9 +8,12 @@ Todo:
 - [x] implement focusing on task-list from any webpage via shortcuts
 - [ ] see if you covered all of the functionalities from original GTask app
 - [x] keyboard shortcuts must work on entire window
+- [ ] implement styling
+- [ ] create instructions
+- [ ] hide api key? https://www.reddit.com/r/webdev/comments/7h9xb6/is_there_a_way_for_me_to_hide_my_google_api_key/
 
 Current:
 - [x] implement reordering
 - [x] implement switching visibility of completed/active tasks
-- [ ] optimize/cache key events and http requests
 - [ ] clear and refactor the codebase
+- [ ] optimize/cache key events and http requests
--- src/App.js
+++ src/App.js
@@ -5,32 +5,9 @@ import React, {
 import { css } from 'styled-components';
 import 'styled-components/macro';
 import GTasks from './components/g-tasks/g-tasks.js';
+import loadGapi from './util/load-gapi.js';
 
 let gapi;
-
-const GAPI_URL = 'https://apis.google.com/js/api.js';
-const CLIENT_ID = '748268010155-9vu3642be1b9ktf230uedab0m63i60ob.apps.googleusercontent.com';
-const API_KEY = 'AIzaSyBGm2a7dU4Ae_g5kCuYkCJyL8xiIJA6jMM';
-const DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/tasks/v1/rest'];
-const SCOPES = 'https://www.googleapis.com/auth/tasks';
-
-const loadGapi = () => new Promise((resolve, reject) => {
-
-    const script = document.createElement('script');
-    script.src = GAPI_URL;
-    script.onload = () => {
-
-        gapi = window.gapi;
-        gapi.load('client:auth2', { callback: resolve, onerror: reject });
-    };
-    document.body.appendChild(script);
-});
-const initGapiClient = () => gapi.client.init({
-    apiKey: API_KEY,
-    clientId: CLIENT_ID,
-    discoveryDocs: DISCOVERY_DOCS,
-    scope: SCOPES
-});
 
 const mainCss = css`
     position: fixed;
@@ -78,10 +55,10 @@ function App() {
     useEffect(function loadGapiAndSetState() {
 
         loadGapi()
-            .then(initGapiClient)
-            .then(() => {
+            .then((initializedGapi) => {
 
                 setIsLoading(false);
+                gapi = initializedGapi;
                 if (gapi.auth2.getAuthInstance().isSignedIn.get()) {
                     setIsSignedIn(true);
                 }
