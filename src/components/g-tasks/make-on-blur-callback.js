c6940c6b82f26f6ca68abab39e3d450972c5c644
DarkMannn <mdarko1993@gmail.com>
2019-09-25 00:07:01 +0200

    Added missed error handling on onBlurCallback function

--- src/components/g-tasks/make-on-blur-callback.js
+++ src/components/g-tasks/make-on-blur-callback.js
@@ -6,34 +6,39 @@ const MakeOnBlurCallback = (
     GapiTasks
 ) => async (newTitle) => {
 
-    dispatch(actionCreators.toggleIsEditingActive(false));
+    try {
+        dispatch(actionCreators.toggleIsEditingActive(false));
 
-    if (isNextBlurInsertion) {
-        await GapiTasks.createTask(tasklist.id, { title: newTitle });
+        if (isNextBlurInsertion) {
+            await GapiTasks.createTask(tasklist.id, { title: newTitle });
+            const tasks = await GapiTasks.loadTasks(tasklist.id, showCompleted);
+            dispatch(actionCreators.reloadTasks(tasks));
+            return;
+        }
+
+        const updatedTask = items[cursor - 1];
+        const previousTask = items[cursor - 2];
+
+        const shouldNotUpdate = updatedTask.title === newTitle;
+        if (shouldNotUpdate) {
+            return;
+        }
+
+        updatedTask.title = newTitle;
+        await GapiTasks.updateTask(
+            tasklist.id,
+            updatedTask.id,
+            updatedTask
+        );
+        if (previousTask && previousTask.id) {
+            await GapiTasks.moveTask(tasklist.id, updatedTask.id, previousTask && previousTask.id);
+        }
         const tasks = await GapiTasks.loadTasks(tasklist.id, showCompleted);
         dispatch(actionCreators.reloadTasks(tasks));
-        return;
     }
-
-    const updatedTask = items[cursor - 1];
-    const previousTask = items[cursor - 2];
-
-    const shouldNotUpdate = updatedTask.title === newTitle;
-    if (shouldNotUpdate) {
-        return;
+    catch (err) {
+        dispatch(actionCreators.toggleHasErrored());
     }
-
-    updatedTask.title = newTitle;
-    await GapiTasks.updateTask(
-        tasklist.id,
-        updatedTask.id,
-        updatedTask
-    );
-    if (previousTask && previousTask.id) {
-        await GapiTasks.moveTask(tasklist.id, updatedTask.id, previousTask && previousTask.id);
-    }
-    const tasks = await GapiTasks.loadTasks(tasklist.id, showCompleted);
-    dispatch(actionCreators.reloadTasks(tasks));
 };
 
 export default MakeOnBlurCallback;
